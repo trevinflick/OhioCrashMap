5b28143664b45fa3e23f5f90f324c1638ba36fdd
Trevin Flickinger <twflick@gmail.com>
2021-05-13 21:50:48 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(zipcodeR)
 
-clean_data <- read_csv('data/clean_data.csv')
+clean_data <- read_csv('clean_data.csv')
 
 ohio <- search_state('OH')
 ohio_county <- ohio$county %>% unique()
